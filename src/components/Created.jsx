eda4f049e0a5be37d6e51e451f88916cb6de5cdb
acer <abubakrbaxriddinov42@gmail.com>
2024-10-13 10:21:15 +0500

    ok

--- src/components/Created.jsx
+++ src/components/Created.jsx
@@ -6,7 +6,7 @@ const Created = () => {
   return (
     <div className="created-by">
       <div className='created-by-1'>
-        <h1 className="created-by-title">Designed With Care <br />Created With Excellence <br /> By CODE CRAFTS TEAM</h1>
+        <h1 className="created-by-title">Designed With Care <br />Created With Excellence <br /> By "CODE CRAFTS TEAM"</h1>
       </div>
       <img src={logo} alt="Code Crafts Logo" className="created-by-logo" />
     </div>
